a2c11542b9e07bac25cea51726ab34d18e6ef251
Ryan Miller <rmiller@marinsoftware.com>
2012-12-04 22:35:55 -0800

    really based on real months this time.  honest

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,7 +11,10 @@ define(['d3'], function(d3) {
         currentDay.setDate(currentDay.getDate()+1);
         days.push({date:thisDay});
         if(thisDay.getMonth() !== currentDay.getMonth()) {
-            months.push({month: thisDay.getMonth(), days: thisDay.getDate()});
+            months.push({
+                startDate: new Date(today.getFullYear(), thisDay.getMonth(), 1),
+                endDate: new Date(today.getFullYear(), thisDay.getMonth(), thisDay.getDate())
+            });
         }
     }
 
@@ -21,14 +24,15 @@ define(['d3'], function(d3) {
         .domain(angles)
         .range([-Math.PI, Math.PI]);
 
-    // create scale for arc angle and days of year
-    var daysArc = d3.scale.linear()
-        .domain([0, days.length - 1])
+    // create scale for arc angle and epoch time
+    var yearArc = d3.scale.linear()
+        .domain([days[0].date.getTime(), days[days.length-1].date.getTime()])
         .range(angles);
 
-    var monthsArc = d3.scale.linear()
-        .domain([0, months.length-1])
-        .range(angles);
+    // create scale for arc angle and days of year
+    var daysArc = d3.scale.linear()
+        .domain([0, days.length-1])
+        .range([0, 359]);
 
     var body = $('body');
 
@@ -43,21 +47,24 @@ define(['d3'], function(d3) {
     g.selectAll("g.day").data(days)
         .enter().append('g').attr('class', 'day')
         .attr("transform", function (d, i) {
-            return "rotate(" + daysArc(i) +
+            return "rotate(" + yearArc(d.date.getTime()) +
                 ")translate(" + 300 + ")";
         }).append('circle').attr('fill', "#333").attr('r', 1.5);
 
-    var arc = d3.svg.arc()
+    var monthArc = d3.svg.arc()
         .innerRadius(274)
         .outerRadius(275)
-        .startAngle(function(d,i){
-            var degrees = monthsArc(i);
+        .startAngle(function(d){
+            var degrees = yearArc(d.startDate);
             var r = radians(degrees);
             return r;
         })
         .endAngle(function(d,i){
-            var degrees = monthsArc(i+1);
-            var r = radians(degrees-1);
+            var degrees = yearArc(d.endDate);
+            if (i === 11) {
+                degrees -= 1;
+            }
+            var r = radians(degrees);
             return r;
         });
 
@@ -65,5 +72,9 @@ define(['d3'], function(d3) {
         .enter().append('g').attr('class', 'month')
         .attr("transform", function (d, i) {
             return "translate(" + 0 + ")rotate(180)";
-        }).append('path').attr('d', arc).fill('#333');
+        }).append('path').attr('d', monthArc).attr('fill', '#333');
+
+    d3.json('data/97008_events.json', function(result) {
+        //g.selectAll
+    });
 });
