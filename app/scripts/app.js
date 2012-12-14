fa2b25f8c4d54b582a72bc805bbd80674c76087d
Ryan Miller <rmiller@marinsoftware.com>
2012-12-13 22:22:35 -0800

    added color scales to temp

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,22 +2,71 @@ define(['d3','underscore'], function(d3,_) {
     "use strict";
 
 
+//    var tempColors = [
+//        //{zone: '3a', minTemp: -40, color: 0xE2A2D7},
+//        //{zone: '3b', minTemp: -35, color: 0xBFACE3},
+//        //{zone: '4a', minTemp: -30, color: 0x96B5E5},
+//        //{zone: '4b', minTemp: -25, color: 0x6CBCDD},
+//        //{zone: '5a', minTemp: -20, color: 0x49C0CC},
+//        //{zone: '5b', minTemp: -15, color: 0x3CC1B4},
+//        //{zone: '6a', minTemp: -10, color: 0x4DC098},
+//        //{zone: '6b', minTemp: -5, color: 0x68BD7C},
+//        {zone: '7a', minTemp: 0, color: "#85B862"},
+//        {zone: '7b', minTemp: 5, color: "#A1B04F"},
+//        {zone: '8a', minTemp: 10, color: "#BBA746"},
+//        {zone: '8b', minTemp: 15, color: "#D19C48"},
+//        {zone: '9a', minTemp: 20, color: "#E39054"},
+//        {zone: '9b', minTemp: 25, color: "#ED8667"},
+//        {zone: '10a', minTemp: 30, color: "#E39054"},
+//        {zone: '10b', minTemp: 35, color: "#ED8667"}
+//    ];
+
     var tempColors = [
-                        0xE2A2D7,
-                        0xBFACE3,
-                        0x96B5E5,
-                        0x6CBCDD,
-                        0x49C0CC,
-                        0x3CC1B4,
-                        0x4DC098,
-                        0x68BD7C,
-                        0x85B862,
-                        0xA1B04F,
-                        0xBBA746,
-                        0xD19C48,
-                        0xE39054,
-                        0xED8667
-                     ];
+        {minTemp: 0, color: "#96BAEA"},
+        {minTemp: 5, color: "#7FBEE7"},
+        {minTemp: 10, color: "#69C0E0"},
+        {minTemp: 15, color: "#55C2D7"},
+        {minTemp: 20, color: "#46C4CC"},
+        {minTemp: 25, color: "#3DC4BF"},
+        {minTemp: 30, color: "#3FC4B1"},
+        {minTemp: 35, color: "#47C3A2"},
+        {minTemp: 40, color: "#54C293"},
+        {minTemp: 45, color: "#62C084"},
+        {minTemp: 50, color: "#71BD75"},
+        {minTemp: 55, color: "#80BA68"},
+        {minTemp: 60, color: "#8FB65C"},
+        {minTemp: 65, color: "#9DB252"},
+        {minTemp: 70, color: "#ABAD4B"},
+        {minTemp: 75, color: "#B8A846"},
+        {minTemp: 80, color: "#C4A245"},
+        {minTemp: 85, color: "#CF9C47"},
+        {minTemp: 90, color: "#D8964C"},
+        {minTemp: 95, color: "#E08F53"},
+        {minTemp: 100, color: "#E68A5B"},
+        {minTemp: 105, color: "#EB8566"} ];
+//
+//    var tempColors = [
+//        {minTemp: 0, color: "#F9B5F0"},
+//        {minTemp: 5, color: "#DEBCF9"},
+//        {minTemp: 10, color: "#C1C2FD"},
+//        {minTemp: 15, color: "#A2C8FC"},
+//        {minTemp: 20, color: "#83CDF6"},
+//        {minTemp: 25, color: "#66D0EC"},
+//        {minTemp: 30, color: "#4ED2DD"},
+//        {minTemp: 35, color: "#42D2CC"},
+//        {minTemp: 40, color: "#46D2B8"},
+//        {minTemp: 45, color: "#55D0A3"},
+//        {minTemp: 50, color: "#68CE8E"},
+//        {minTemp: 55, color: "#7DCA7B"},
+//        {minTemp: 60, color: "#90C569"},
+//        {minTemp: 65, color: "#A4BF5A"},
+//        {minTemp: 70, color: "#B6B850"},
+//        {minTemp: 75, color: "#C7B14A"},
+//        {minTemp: 80, color: "#D7A94B"},
+//        {minTemp: 85, color: "#E4A050"},
+//        {minTemp: 90, color: "#EF9859"},
+//        {minTemp: 95, color: "#F69066"}
+//    ]
 
     var body = $('body');
 
@@ -113,7 +162,7 @@ define(['d3','underscore'], function(d3,_) {
             .attr("height", body.outerHeight())
             .append("g")
             .attr("transform", "translate(" + body.outerWidth()/2 + "," + body.outerHeight()/2 + ")")
-            .append("g").attr("transform", "scale(1)");
+            .append("g").attr("transform", "rotate(-90)scale(1)");
 
         // season mark
         g.append('g').attr('class','axis')
@@ -243,8 +292,6 @@ define(['d3','underscore'], function(d3,_) {
             .attr('d', growArc())
             .attr("transform", "rotate(" + (-yearArc(events.frosts.start)+frostAngle) + ")");
 
-        //g.append('use').attr('href', '#growArc');
-
         g.append('g').attr('class', 'grow')
             .append('path').attr('d', growArc())
             .attr("transform", "rotate(" + (-yearArc(events.frosts.start)+frostAngle) + ")");
@@ -257,8 +304,7 @@ define(['d3','underscore'], function(d3,_) {
             .attr('baseline-shift', '2')
             .text('Growing Season ⇾');
 
-
-        // temperature marks
+        // temperature scales
         var maxTemp = d3.max(stats, function(d){
             return d['TMAX-MAX'];
         });
@@ -271,10 +317,12 @@ define(['d3','underscore'], function(d3,_) {
             .domain([0, maxTemp-minTemp])
             .range([0,tempWidth]);
 
+        // temperature marks
+        var clipPath = g.append("defs").append('clipPath').attr('id', 'tempMarks')
 
-        g.append("g").attr('class', 'temps').selectAll("g.temp").data(stats)
-            .enter().append('g').attr('class', 'temp')
-            .append('rect')
+        clipPath.selectAll("rect.temp").data(stats)
+            .enter()
+            .append('rect').attr('class', 'temp')
             .attr('width', function(d){ return tempScale((d.TMAX- d.TMIN));})
             .attr('height', 4)
             .attr("transform", function (d, i) {
@@ -283,11 +331,52 @@ define(['d3','underscore'], function(d3,_) {
                     "translate(" + (innerTempRadius + tempScale(d.TMIN)) + ")";
             });
 
+        clipPath.selectAll("circle.maxTemp").data(stats)
+            .enter()
+            .append('circle').attr('class', 'maxTemp')
+            .attr('r', 1.5)
+            .attr("transform", function (d, i) {
+                var date = new Date(today.getFullYear(), d.MO, d.DY, 0, 0, 0, 0);
+                return "rotate(" + yearArc(date.getTime()) + ")" +
+                    "translate(" + (innerTempRadius + tempScale(d['TMAX-MAX'])) + ")";
+            });
+
+        clipPath.selectAll("circle.minTemp").data(stats)
+            .enter()
+            .append('circle').attr('class', 'minTemp')
+            .attr('r', 1.5)
+            .attr("transform", function (d, i) {
+                var date = new Date(today.getFullYear(), d.MO, d.DY, 0, 0, 0, 0);
+                return "rotate(" + yearArc(date.getTime()) + ")" +
+                    "translate(" + (innerTempRadius + tempScale(d['TMIN-MIN'])) + ")";
+            });
+
+        // temperature colors
+        g.append('g').attr('class', 'tempColors')
+            .selectAll('g.tempColor').data(tempColors).enter()
+            .append('g').attr('class', 'tempColor')
+            .append('path').attr('fill', function(d){
+                return d.color;
+            })
+            .attr('style', 'clip-path: url(#tempMarks);')
+            .attr('d', d3.svg.arc()
+                .innerRadius(function(d){
+                    return innerTempRadius + tempScale(d.minTemp);
+                })
+                .outerRadius(function(d){
+                    return innerTempRadius + tempScale(d.minTemp+5);
+                })
+                .startAngle(0)
+                .endAngle(360));
+
+
+
 //        g.append('g').attr('class','axis')
 //            .append('circle').attr('r', innerTempRadius);
 
-        g.append('g').attr('class','axis')
-            .append('circle').attr('r', innerTempRadius + tempScale(32));
+        // axis line for freezing
+//        g.append('g').attr('class','axis')
+//            .append('circle').attr('r', innerTempRadius + tempScale(32));
 
 //
 //        g.append('g').attr('class','axis')
